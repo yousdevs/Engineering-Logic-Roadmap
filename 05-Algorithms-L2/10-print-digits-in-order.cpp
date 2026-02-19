df83329ec800c7bf34539963d19952171c5a16a7
Youssef Asaad <yousdevs@gmail.com>
2026-02-19 22:26:39 +0300

    feat(05-Algorithms-L2): 10-print-digits-in-order

---
+++ 05-Algorithms-L2/10-print-digits-in-order.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int readPositiveNumber(std::string message) {
+	int number = 0;
+	do
+	{
+		std::cout << message << std::endl;
+		std::cin >> number;
+	} while (number <= 0);
+	return number;
+}
+int reverseNumber(int number) {
+	// The idea is to take the last digit of the number and add it to the reversed number,
+	// then remove the last digit from the original number.
+	int reversed = 0;
+	while (number > 0) {
+		reversed *= 10;
+		reversed += number % 10;
+		number /= 10;
+	}
+	return reversed;
+}
+void printDigitsInOrder(int number) {
+	int reminder = 0;
+	while (number > 0) {
+		reminder = number % 10;
+		std::cout << reminder << std::endl;
+		number /= 10;
+	}
+}
+
+int main() {
+	printDigitsInOrder(reverseNumber(readPositiveNumber("Please enter a positive number: ")));
+	return 0;
+}
