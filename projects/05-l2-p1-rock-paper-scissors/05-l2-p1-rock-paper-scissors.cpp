b08a364592ed818c2b99e793e7749f1ec430f77a
Youssef Asaad <yousdevs@gmail.com>
2026-02-21 06:59:59 +0300

    feat(l2-p1): implement asking for choice functionality and ui

--- projects/05-l2-p1-rock-paper-scissors/05-l2-p1-rock-paper-scissors.cpp
+++ projects/05-l2-p1-rock-paper-scissors/05-l2-p1-rock-paper-scissors.cpp
@@ -15,13 +15,62 @@ short askForRoundsCount() {
     return readPositiveNumberInRange(1, 10, "How Many Rounds 1 to 10 ?");
 }
 
+enum enChoice { Rock = 1, Paper = 2, Scissors = 3 };
+
+enChoice getChoiceFrom(int number) {
+    switch (number) {
+    case enChoice::Rock:
+        return enChoice::Rock;
+    case enChoice::Paper:
+        return enChoice::Paper;
+    case enChoice::Scissors:
+        return enChoice::Scissors;
+    }
+}
+
+enChoice askForPlayerChoice(short currentRound){
+    std::cout << "Round [" << currentRound << "] begins:" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Your Choice: "
+        << "[" << enChoice::Rock << "], "
+        << "[" << enChoice::Paper << "], "
+        << "[" << enChoice::Scissors << "] ? ";
+
+    short input = readPositiveNumberInRange(1, 3, "");
+    return getChoiceFrom(input);
+}
+
+// Returns: A random integer between From and To (inclusive).
+int getRandomNumberInRange(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+enChoice askForComputerChoice() {
+    int randomNumber = getRandomNumberInRange(1, 3);
+    return getChoiceFrom(randomNumber);
+}
+
 void startGame() {
+
     short rounds = askForRoundsCount();
+    
+    for (int i = 0; i < rounds; i++) {
+        enChoice playerChoice = askForPlayerChoice(i + 1);
+        enChoice computerChoice = askForComputerChoice();
+        std::cout << "\nPlayer choice: " << playerChoice << ", Coputer: " << computerChoice;
+    }
 }
 
 int main()
 {
+    // Seed the random number generator with the current time.
+    // This ensures that we get a different sequence of random numbers on each run.
+    srand((unsigned)time(NULL));
+
     startGame();
-    
+
+    return 0;
 }
 
