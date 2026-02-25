b5aec6a05e25777eb16cc4a8d331b967e7ff058c
Youssef Asaad <yousdevs@gmail.com>
2026-02-25 13:10:21 +0300

    fix(07-l3): 29-count-small-capital-letters

---
+++ 07-Algorithms-L3/29-count-small-capital-letters.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+char invertCharacter(char c) {
+	if (std::isupper(c)) {
+		return std::tolower(c);
+	}
+	else {
+		return std::toupper(c);
+	}
+}
+
+
+void printCounts(std::string str) {
+	int length = str.length();
+	printf("\nString Length is: %d", length);
+	int upperLetters = 0, smallLetters = 0;
+	for (int i = 0; i < length; i++) {
+		if (std::isupper(str[i]) && str[i] != ' ') {
+			upperLetters++;
+		}
+		else {
+			smallLetters++;
+		}
+	}
+	printf("\nUppers: %d \nLowers: %d", upperLetters, smallLetters);
+}
+
+int main() {
+
+	std::string str = "MUHAMMED abu hadhud";
+	printCounts(str);
+
+
+	return 0;
+}
