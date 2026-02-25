8a79e732e1fe357370de5f92b79f8f4fdd9c78dd
Youssef Asaad <yousdevs@gmail.com>
2026-02-25 16:08:44 +0300

    feat(07-l3): 36-count-each-word-in-string

---
+++ 07-Algorithms-L3/36-count-each-word-in-string.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+
+
+std::string readString() {
+	std::string str = "";
+	std::getline(std::cin, str);
+	return str;
+}
+
+void printWord(std::string str, short startIndex, short endIndex) {
+	std::string word = "";
+	for (short i = startIndex; i <= endIndex; i++) {
+		word += str[i];
+	}
+	std::cout << word << std::endl;
+}
+
+void printEachWordInString(std::string str) {
+
+	short wordStartPos = 0;
+	short wordEndPos = 0;
+	for (short i = 0; i < str.length(); i++) {
+		if (str[i] == ' ') {
+			printWord(str, wordStartPos, i - 1);
+			wordStartPos = i + 1;
+		}
+		if (i == str.length() - 1) {
+			printWord(str, wordStartPos, i);
+		}
+	}
+}
+
+void PrintEachWordInString(std::string S1)
+{
+	std::string delim = " "; // delimiter
+	std::cout << "\nYour string wrords are: \n\n";
+	short pos = 0;
+	std::string sWord; // define a string variable
+	// use find() function to get the position of the delimiters
+	while ((pos = S1.find(delim)) != std::string::npos)
+	{
+		sWord = S1.substr(0, pos); // store the word
+		if (sWord != "")
+		{
+			std::cout << sWord << std::endl;
+		}
+		S1.erase(0, pos + delim.length()); /* erase() until
+		positon and move to next word. */
+	}
+	if (S1 != "")
+	{
+		std::cout << S1 << std::endl; // it print last word of the string.
+	}
+}
+
+short countWords(std::string str) {
+	short counter = 0;
+	short pos = 0;
+	std::string word = "";
+	std::string delim = " ";
+	while ((pos = str.find(delim)) != std::string::npos) {
+		word = str.substr(0, pos);
+		if (word != "") {
+			counter++;
+		}
+		str.erase(0, pos + delim.length());
+	}
+	if (str != "") counter++;
+	return counter;
+}
+
+int main() {
+	std::string str = readString();
+
+	printf("\nThe words count is %d", countWords(str));
+	return 0;
+}
