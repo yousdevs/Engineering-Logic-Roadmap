f11f53954297f4fa31ef8e142b990bd21f3d8ed2
Youssef Asaad <yousdevs@gmail.com>
2026-02-26 17:21:34 +0300

    feat(bank1): implement client menu screen UI

--- projects/07-l3-p1-bank1/07-l3-p1-bank1.cpp
+++ projects/07-l3-p1-bank1/07-l3-p1-bank1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
+#include <string>
 
 enum enMainMenuOptions {
     SHOW_CLIENT_LIST = 1,
@@ -9,6 +10,14 @@ enum enMainMenuOptions {
 	UPDATE_CLIENT_INFO = 4,
 	FIND_CLIENT = 5,
 	EXIT = 6,
+};
+
+struct stClient {
+	std::string accountID = "";
+	std::string pinCode = "";
+	std::string fullName = "";
+	std::string phoneNumber = "";
+	double accountBalance = 0.0;
 };
 
 struct stMenuItem {
@@ -32,6 +41,48 @@ void showMenu(std::vector<stMenuItem> menu, std::string headerLabel = "") {
 	std::cout << "Please choose an option: ";
 }
 
+void showClientsScreen(std::vector<stClient> clients) {
+	
+	std::string clientList = "Client List (" + std::to_string(clients.size()) + ") Client(s).\n";
+	std::string line = std::string(120, '-') + "\n";
+
+	std::cout << "\n" << std::setw(75) << clientList;
+	std::cout << line;
+
+	// Header
+	std::cout << "|"
+		<< std::left << std::setw(19) << "Account ID"
+		<< "|"
+		<< std::left << std::setw(19) << "Pin Code"
+		<< "|"
+		<< std::left << std::setw(29) << "Full Name"
+		<< "|"
+		<< std::right << std::setw(19) << "Phone Number"
+		<< "|"
+		<< std::right << std::setw(14) << "Balance"
+		<< "|\n";
+
+	std::cout << line;
+
+	// Data
+	for (stClient& client : clients) {
+		std::cout << "|"
+			<< std::left << std::setw(19) << client.accountID
+			<< "|"
+			<< std::left << std::setw(19) << client.pinCode
+			<< "|"
+			<< std::left << std::setw(29) << client.fullName
+			<< "|"
+			<< std::right << std::setw(19) << client.phoneNumber
+			<< "|"
+			<< std::right << std::setw(14) << client.accountBalance
+			<< "|\n";
+	}
+
+	std::cout << line;
+	std::cout << "Press any key to go back to main menu...";
+}
+
 int main()
 {
 	std::vector<stMenuItem> mainMenu = {
@@ -43,5 +94,13 @@ int main()
 		{ EXIT, "Exit"},
 	};
 	showMenu(mainMenu, "Main Menu");
+
+	std::vector<stClient> clients{
+		{"1", "1234", "Ahmed Omar", "0543334433", 2300},
+		{"1", "1234", "Ahmed Omar", "0543334433", 2300},
+		{"1", "1234", "Ahmed Omar", "0543334433", 2300},
+	};
+	showClientsScreen(clients);
+	return 0;
     
 }
