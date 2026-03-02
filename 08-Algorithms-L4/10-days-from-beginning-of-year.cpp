9c238e2711cc42ae85c4e556525dc3dd92377c8e
Youssef Asaad <yousdevs@gmail.com>
2026-03-02 18:54:11 +0300

    feat(08-l4): 10-days-from-beginning-of-year

---
+++ 08-Algorithms-L4/10-days-from-beginning-of-year.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "my-lib.h"
+#include <iomanip>
+
+bool isLeapYear(int year)
+{
+    return (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0));
+}
+
+short totalDaysInMonth(int year, short month)
+{
+    if (month < 1 || month > 12)
+        return 0;
+
+    const short daysPerMonth[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    return (month == 2 && isLeapYear(year)) ? 29 : daysPerMonth[month - 1];
+}
+
+short calculateTotalDaysFromBeginningOfYear(short year, short month, short day) {
+    if (month < 1 || month > 12)
+        return 0;
+
+    int maxDays = totalDaysInMonth(year, month);
+
+    if (day < 1 || day > maxDays)
+        return 0;
+
+    short total = 0;
+    for (short i = 1; i < month; i++) {
+        total += totalDaysInMonth(year, i);
+    }
+    total += day;
+    return total;
+}
+
+void printDaysFromBeginningOfYear(short year, short month, short day) {
+    std::cout << "Total days From Beginning of the year: " << calculateTotalDaysFromBeginningOfYear(year, month, day);
+}
+
+
+int main()
+{
+    int year = mylib::readValidInteger("Please enter a year (e.g., 2000): ");
+    int month = mylib::readValidInteger("Please enter a month (e.g., 2): ");
+    int day = mylib::readValidInteger("Please enter a day (e.g., 2): ");
+    printDaysFromBeginningOfYear(year, month, day);
+}
