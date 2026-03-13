08967a0fa1fc95d3d98b3d44a3362d5aa60494b9
Youssef Asaad <yousdevs@gmail.com>
2026-03-14 01:27:58 +0300

    feat(domain): add IUserRepository interface

---
+++ projects/11-oop-p3-bank/domain/repositories/IUserRepository.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <optional>
+
+#include "../entities/User.hpp"
+
+class IUserRepository {
+ public:
+  virtual ~IUserRepository() = default;
+
+  virtual std::vector<User> findAll() const = 0;
+
+  /**
+   * @brief Finds a specific User by their unique username.
+   * @param username The username ID to search for.
+   * @return An optional containing the User if found, std::nullopt
+   * otherwise.
+   */
+  virtual std::optional<User> findByUsername(
+      const std::string& username) const = 0;
+
+  virtual void save(const User& user) = 0;
+
+  virtual void remove(const std::string& username) = 0;
+};
