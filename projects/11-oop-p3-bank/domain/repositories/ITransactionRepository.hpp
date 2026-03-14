cad71bd19df98f65fda28f956111afa224bec335
Youssef Asaad <yousdevs@gmail.com>
2026-03-14 03:37:53 +0300

    feat(domain): add ITransactionRepository interface

---
+++ projects/11-oop-p3-bank/domain/repositories/ITransactionRepository.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vector>
+
+#include "../domain/entities/Transaction.hpp"
+
+
+class ITransactionRepository {
+
+ public:
+
+	virtual ~ITransactionRepository() = default;
+
+	virtual void save(const Transaction& transaction) = 0;
+
+	virtual std::vector<Transaction> findAll() const = 0;
+
+	virtual std::vector<Transaction> findByAccountId(
+            const std::string& accountId) const = 0;
+
+};
