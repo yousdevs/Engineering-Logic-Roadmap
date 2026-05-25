cfdbb46bfe106e26c0a8986e87b77616d8386608
Youssef Asaad <yousdevs@gmail.com>
2026-05-25 19:24:16 +0300

    feat: add raw buffer management with allocate/deallocate and destroy helpers

--- projects/13-p4-dynamic-array/include/array.hpp
+++ projects/13-p4-dynamic-array/include/array.hpp
@@ -1,1 +1,72 @@
 #pragma once
+
+#include <cstddef>
+#include <initializer_list>
+#include <iterator>
+#include <new>
+#include <stdexcept>
+#include <type_traits>
+#include <utility>
+
+template<typename T>
+class DynamicArray {
+
+    public:
+
+        using value_type      = T;
+        using size_type       = std::size_t;
+        using difference_type = std::ptrdiff_t;
+        using reference       = T&;
+        using const_reference = const T&;
+        using pointer         = T*;
+        using const_pointer   = const T*;
+
+    private:
+
+        void _destroyRange(pointer buffer, size_type count) noexcept {
+
+            if constexpr (!std::is_trivially_destructible_v<T>) {
+
+                for (size_type i = count; i > 0; --i) {
+
+                    buffer[i - 1].~T();
+                }
+            }
+        }
+
+        void _reallocate(size_type new_capacity) {
+
+            pointer   buffer = static_cast<pointer>(::operator new(new_capacity * sizeof(T)));
+            size_type constructed_count = 0;
+
+            try {
+
+                for (size_type i = 0; i < _size; ++i) {
+
+                    new (buffer + i) T(std::move_if_noexcept(_data[i]));
+                    ++constructed_count;
+                }
+            } catch (...) {
+
+                _destroyRange(buffer, constructed_count);
+                ::operator delete(buffer);
+                throw;
+            }
+
+            _destroyRange(_data, _size);
+            ::operator delete(_data);
+
+            _data     = buffer;
+            _capacity = new_capacity;
+        }
+
+        void _destroyAll() noexcept {
+
+            _destroyRange(_data, _size);
+            _size = 0;
+        }
+
+        pointer   _data     = nullptr;
+        size_type _size     = 0;
+        size_type _capacity = 0;
+};
