a8b7dae9abd3d023779188be9896c36ec3591239
Youssef Asaad <yousdevs@gmail.com>
2026-05-25 20:30:19 +0300

    feat: implement default, count, fill, and initializer_list construction

--- projects/13-p4-dynamic-array/include/array.hpp
+++ projects/13-p4-dynamic-array/include/array.hpp
@@ -21,6 +21,64 @@ class DynamicArray {
         using pointer         = T*;
         using const_pointer   = const T*;
 
+        DynamicArray() noexcept : _data(nullptr), _size(0), _capacity(0) {}
+
+        explicit DynamicArray(size_type count) {
+
+            _reallocate(count);
+
+            for (size_type i = 0; i < count; ++i) {
+
+                new (_data + i) T();
+            }
+            _size = count;
+        }
+
+        DynamicArray(size_type count, const_reference value) {
+
+            _reallocate(count);
+
+            for (size_type i = 0; i < count; ++i) {
+
+                new (_data + i) T(value);
+            }
+
+            _size = count;
+        }
+
+        DynamicArray(std::initializer_list<value_type> init) {
+
+            _reallocate(init.size());
+
+            size_type i = 0;
+            for (auto it = init.begin(); it != init.end(); ++it, ++i) {
+
+                new (_data + i) T(*it);
+            }
+            _size = init.size();
+        }
+
+        ~DynamicArray() {
+
+            _destroyAll();
+            ::operator delete(_data);
+        }
+
+        size_type size() const noexcept {
+
+            return _size;
+        }
+
+        bool empty() const noexcept {
+
+            return _size == 0;
+        }
+
+        size_type capacity() const noexcept {
+
+            return _capacity;
+        }
+
     private:
 
         void _destroyRange(pointer buffer, size_type count) noexcept {
--- projects/13-p4-dynamic-array/tests/test_array.cpp
+++ projects/13-p4-dynamic-array/tests/test_array.cpp
@@ -2,7 +2,42 @@
 
 #include <gtest/gtest.h>
 
-TEST(TESTTEST, OneEqualOne) {
+TEST(ConstructionTest, DefaultConstructorIsEmpty) {
 
-    EXPECT_EQ(1, 1);
+    DynamicArray<int> arr;
+
+    EXPECT_EQ(arr.size(), 0);
+    EXPECT_TRUE(arr.empty());
+    EXPECT_EQ(arr.capacity(), 0);
+}
+
+TEST(ConstructionTest, CountConstructorDefaultConstructElements) {
+
+    DynamicArray<int> arr(5);
+
+    EXPECT_EQ(arr.size(), 5);
+    EXPECT_FALSE(arr.empty());
+    EXPECT_EQ(arr.capacity(), 5);
+}
+
+TEST(ConstructionTest, FillConstuctorFillsWithValue) {
+
+    DynamicArray<int> arr(4, 2);
+
+    EXPECT_EQ(arr.size(), 4);
+
+    // TODO accessor
+}
+
+TEST(ConstructionTest, InitializerListConstructsCorrectly) {
+
+    DynamicArray<int> arr = {1, 2, 3};
+    EXPECT_EQ(arr.size(), 3);
+}
+
+TEST(ConstructionTest, DestuctorRunsWithoutLeak) {
+
+    {
+        DynamicArray<int> arr(10, 7);
+    }
 }
