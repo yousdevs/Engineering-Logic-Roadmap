c20316f48a420061164189964f7d1262bfea8098
Youssef Asaad <yousdevs@gmail.com>
2026-08-04 20:51:33 +0300

    refactor(backend): introduce PersonService for centralizing business orchestration

---
+++ projects/19-p1-dvld/backend/Core/Services/PersonService.cs
@@ -0,0 +1,50 @@
+﻿namespace Core.Services;
+
+using Core.DTOs;
+using Core.Entities;
+using Data;
+
+public sealed class PersonService
+{
+
+
+    public async Task<PagedResult<PersonSummary>> GetAllAsync(Page page)
+    {
+
+        int offset = (page.Number - 1) * page.Size;
+
+        var people = new List<PersonSummary>();
+
+        int totalCount = await PersonData.FindAllAsync(
+            offset,
+            page.Size,
+            (
+                personId,
+                nationalNo,
+                firstName,
+                secondName,
+                thirdName,
+                lastName,
+                gender,
+                dateOfBirth,
+                nationality,
+                phoneNumber,
+                email
+            ) => people.Add(new PersonSummary(
+                personId,
+                nationalNo,
+                firstName,
+                secondName,
+                thirdName,
+                lastName,
+                Enum.IsDefined(typeof(Gender), (int)gender) ? ((Gender)gender).ToString()
+                : throw new InvalidOperationException($"Unknown Gender Value: {gender}"),
+                dateOfBirth,
+                nationality,
+                phoneNumber,
+                email
+                )));
+
+        return new PagedResult<PersonSummary>(people, totalCount, page.Number, page.Size);
+    }
+}
